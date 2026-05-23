af7d8f520a2c4cd897c8fb3ac6b699accde7e159
Atanael Cardoso <110429151+atanaelcardoso@users.noreply.github.com>
2026-05-23 15:33:25 -0300

    feat: dando função a userinfo

--- frontend/src/components/UserInfo/index.tsx
+++ frontend/src/components/UserInfo/index.tsx
@@ -1,4 +1,5 @@
-import React  from 'react';
+import { useEffect, useState } from 'react';
+import api from '../../Services/api'; 
 
 import { 
     Container, 
@@ -8,26 +9,72 @@ import {
     Icons, 
     MicIcon, 
     HeadphoneIcon, 
-    SettingsIcon  } from './styles'
+    SettingsIcon  } from './styles';
+interface User {
+  id: number;
+  nickname: string;
+  avatar: string | null;
+}
 
-const UserInfo: React.FC = () => {
+export default function UserInfo() {
+  const [user, setUser] = useState<User | null>(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function fetchUser() {
+      try {
+        const response = await api.get<User[]>('/users');
+        
+        if (response.data.length > 0) {
+          setUser(response.data[0]);
+        }
+      } catch (error) {
+        console.error('Erro ao buscar usuário:', error);
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    fetchUser();
+  }, []);
+
+  if (loading) {
     return (
-        <Container>
-            <Profiler>
-                <Avatar/>
-                <UserData>
-                    <strong>Atanael</strong>
-                    <span>#7777</span>
-                </UserData>
-            </Profiler>
+      <Container>
+        <Profiler>
+          <UserData><strong>Carregando...</strong></UserData>
+        </Profiler>
+      </Container>
+    );
+  }
 
-            <Icons>
-                <MicIcon/>
-                <HeadphoneIcon/>
-                <SettingsIcon/> 
-            </Icons>
-        </Container>
-    )
-};
+  if (!user) {
+    return (
+      <Container>
+        <Profiler>
+          <UserData><strong>Nenhum usuário</strong></UserData>
+        </Profiler>
+      </Container>
+    );
+  }
 
-export default UserInfo;
+  const userTag = `#${String(user.id).padStart(4, '0')}`;
+
+  return (
+    <Container>
+        <Profiler>
+            <Avatar style={{ backgroundImage: user.avatar ? `url(${user.avatar})` : undefined }} />
+            <UserData>
+                <strong>{user.nickname}</strong>
+                <span>{userTag}</span>
+            </UserData>
+        </Profiler>
+
+        <Icons>
+            <MicIcon/>
+            <HeadphoneIcon/>
+            <SettingsIcon/> 
+        </Icons>
+    </Container>
+  );
+}
